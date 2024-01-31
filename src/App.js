96355301acf6c11b9e57f8c5e5433b3b59b41a5f
thanos101201 <ayushsingh10122001@gmail.com>
2024-01-31 23:57:10 +0530

    initiated the clientand added routing code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home'
+import History from './Components/History';
+import Sign from './Components/Sign';
+import Login from './Components/Login';
+import Question from './Components/Question';
+import Otp from './Components/Otp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route exact path='/' element={<Home />} />
+      <Route path='/history' element={<History />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/sign' element={<Sign />} />
+      <Route path='/question' element={<Question />} />
+      <Route path='/otp' element={<Otp />} />
+    </Routes>
   );
 }
 

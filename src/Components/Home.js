dd60a2c6a9d3c90b46d95c536d7d39055d25d762
thanos101201 <ayushsingh10122001@gmail.com>
2024-02-02 09:13:15 +0530

    implemented the code for history, reply and home component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Reply from './Reply'
+import History from './History'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='container'>
+      <div className='row d-flex justify-content-center'>
+        <div className='col-12 col-md-6 d-flex align-items-center'>
+          <Reply />
+        </div>
+        <div className='col-12 col-md-6 d-flex align-items-center'>
+          <History />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/Components/Reply.js
+++ src/Components/Reply.js
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AccordionItem, AccordionBody, AccordionHeader, Accordion, Button, Card, CardBody, CardHeader, CardTitle, Form, FormGroup, Input, Label } from 'reactstrap';
+import { useLocation } from 'react-router-dom';
 
-function Reply() {
+function Reply(props) {
+  const [ replies, setReplies ] = useState([]);
+  const [ id, setId ] = useState("");
+  const [ open, setOpen ] = useState("");
+
+  const toggle = (e) => {
+    if(e === open){
+      setOpen("");
+    }
+    else{
+      setOpen(e);
+    }
+  }
+
+  const { state } = useLocation();
+
+  useEffect(() => {
+    let id = state.id
+    setId(id);
+    axios.get('http://localhost:3001/reply', {
+      headers: {
+        questionId : id
+      }
+    }).then((response) => {
+      if(response.data.message === 'Replies are here'){
+        setReplies(response.data.data);
+      }
+    }).catch((eror) => {
+      alert(eror.message);
+    });
+  }, []);
+
+  const renderReplies = () => {
+    if(replies.length === 0){
+      return(
+        <div></div>
+      );
+    }
+    else
+    {
+      let i = 0;
+      return replies.map((e, key) => {
+        i = i + 1;
+        return(
+          <AccordionItem key={key}>
+            <AccordionHeader targetId={i}>{e.email}</AccordionHeader>
+            <AccordionBody accordionId={i}>
+              <div className='row d-flex justify-content-center'>
+                <div className='col-12 d-flex align-items-center'>
+                  <p>{e.description}</p>
+                </div>
+              </div>
+              <div className='row d-flex justify-content-center'>
+                <div className='col-12 col-md-6 d-flex align-items-center'>
+                  <Button>Like</Button>
+                </div>
+                <div className='col-12 col-md-6 d-flex align-items-center'>
+                  <Button>Dislike</Button>
+                </div>
+              </div>
+            </AccordionBody>
+          </AccordionItem>
+        );
+      })
+    }
+  }
+
   return (
     <div className='container'>
-        
+      <div className='row d-flex justify-content-center'>
+        <div className='col-12 col-md-8 d-flex align-items-center'>
+          <Button>Join</Button>
+        </div>
+      </div>
+        <div className='row d-flex justify-content-center'>
+          <div className='col-12 d-flex align-items-center'>
+            <Accordion open={open} toggle={toggle}>
+              {renderReplies()}
+            </Accordion>
+          </div>
+        </div>
     </div>
   )
 }
